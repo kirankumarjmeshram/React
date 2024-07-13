2791e8f6b8eac82faae2d00e47053c36fbb37de0
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-07-13 21:28:12 +0530

    created react app

---
+++ basic/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UsingFetch from './concepts/data_fetching/UsingFetch';
+
+function App() {
+  return (
+    <div className="App">
+     <UsingFetch/>
+    </div>
+  );
+}
+
+export default App;
